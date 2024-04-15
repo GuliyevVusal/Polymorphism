183bf2109a31d640815c41694dea36b69d62c318
Vusal <143846119+GuliyevVusal@users.noreply.github.com>
2024-04-16 01:06:42 +0400

    Update CustomerManager.java

--- src/CustomerManager.java
+++ src/CustomerManager.java
@@ -1,18 +1,13 @@
 public class CustomerManager {
     private BaseLogger logger;
 
-    public CustomerManager(BaseLogger logger) { //Actually there is no Base
-        // Logger here there is a DatabaseLogger the Base Logger only keeps its reference.
+    public CustomerManager(BaseLogger logger) { 
         this.logger = logger;
     }
 
     public void add() {
         System.out.println("customer added: ");
         this.logger.log("Log message: ");
-
-        //ForExample
-        //Making new means I am connected to this class.
-        //bunun yerine Base Logger field yaratmak attribute
 
 //        DatabaseLogger logger = new DatabaseLogger();
 //        logger.log("Log message: ");
